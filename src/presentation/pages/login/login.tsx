d423db52ee917a3b1267d62d88fba478c7f34bc8
Alan <allan_net@live.com>
2022-03-26 15:40:13 -0300

    feat: ensure Login calls Authentication with correct values

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -3,12 +3,14 @@ import Context from '@/presentation/contexts/form/form-context'
 import Styles from './login-styles.scss'
 import { Footer, Input, FormStatus, LoginHeader } from '@/presentation/components'
 import { Validation } from '@/presentation/protocols/validation'
+import { Authentication } from '@/domain/usecases'
 
 type Props = {
   validation: Validation
+  authentication: Authentication
 }
 
-const Login: React.FC<Props> = ({ validation }: Props) => {
+const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
   const [state, setState] = useState({
     isLoading: false,
     email: '',
@@ -32,18 +34,22 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
     }))
   }, [state.password])
 
-  const handleSubmit = (event: React.SyntheticEvent): void => {
+  const handleSubmit = async (event: React.SyntheticEvent): Promise<void> => {
     event.preventDefault()
     setState((prevState) => ({
       ...prevState,
       isLoading: true
     }))
+    await authentication.auth({
+      email: state.email,
+      password: state.password
+    })
   }
 
   return (
     <div className={Styles.login}>
       <LoginHeader />
-      <Context.Provider value={ { state, setState }}>
+      <Context.Provider value={{ state, setState }}>
         <form className={Styles.form} onSubmit={handleSubmit}>
           <h2>Login</h2>
           <Input type="email" name="email" placeholder="Digite seu e-mail" />
