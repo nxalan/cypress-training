96cb42c6daafdb6f0db85bbc2af8c71a7e4ffb61
Alan <allan_net@live.com>
2022-04-17 20:34:16 -0300

    test: ensure Login present error if SaveAccessToken throws

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -182,4 +182,14 @@ describe('Login Component', () => {
     expect(history.length).toBe(2)
     expect(history.location.pathname).toBe('/signup')
   })
+
+
+  test('Should present error if SaveAccessToken fails', async () => {
+    const { sut, saveAccessTokenMock } = makeSut()
+    const error = new InvalidCredentialsError()
+    jest.spyOn(saveAccessTokenMock, 'save').mockReturnValueOnce(Promise.reject(error))
+    await simulateValidSubmit(sut)
+    testElementText(sut, 'main-error', error.message)
+    testErrorWrapChildCount(sut, 1)
+  })
 })
