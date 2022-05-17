98daacb8850770adb6f7d195526b984628eece8e
Alan <allan_net@live.com>
2022-05-17 18:18:16 -0300

    refactor: adapt layout to mobile

--- src/presentation/components/login-header/login-header.tsx
+++ src/presentation/components/login-header/login-header.tsx
@@ -4,7 +4,7 @@ import Styles from './login-header-styles.scss'
 
 const LoginHeader: React.FC = () => {
   return (
-    <header className={Styles.header}>
+    <header className={Styles.headerWrap}>
       <Logo />
       <h1>Enquetes</h1>
     </header>
