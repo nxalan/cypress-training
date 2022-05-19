21c4fe2fcf08b4972d43e62bea409a4aae21d0bd
Alan <allan_net@live.com>
2022-05-19 20:36:21 -0300

    feat: add surveyList composition

--- src/main/factories/pages/signup/signup-factory.tsx
+++ src/main/factories/pages/signup/signup-factory.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { SignUp } from '@/presentation/pages'
-import { makeRemoteAddAccount } from '@/main/factories/usecases/add-account/remote-add-account-factory'
 import { makeSignUpValidation } from './signup-validation-factory'
-
+import { makeRemoteAddAccount } from '@/main/factories/usecases'
 export const makeSignUp: React.FC = () => {
   return (
     <SignUp
