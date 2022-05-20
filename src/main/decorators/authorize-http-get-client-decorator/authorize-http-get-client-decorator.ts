79cb846c31ace16dadb3c65e8d953c5fc6e533ef
Alan <allan_net@live.com>
2022-05-20 18:35:15 -0300

    refactor: move SUT creation to a factory method

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
@@ -3,6 +3,7 @@ import { HttpGetClient, HttpGetParams, HttpResponse } from '@/data/protocols/htt
 
 export class AuthorizeHttpGetClientDecorator implements HttpGetClient {
   constructor (private readonly getStorage: GetStorage) {}
+
   async get (params: HttpGetParams): Promise<HttpResponse> {
     this.getStorage.get('account')
     return null as any
