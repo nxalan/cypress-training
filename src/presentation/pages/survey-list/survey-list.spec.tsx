3accd9433a21e5ee022f7fe50e975e10c1ddc30e
Alan <allan_net@live.com>
2022-05-23 19:08:20 -0300

    test: ensure logout calls setCurrentAccount with undefined

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -4,6 +4,9 @@ import { SurveyList } from '@/presentation/pages'
 import { LoadSurveyList } from '@/domain/usecases'
 import { mockSurveyListModel } from '@/domain/test'
 import { UnexpectedError } from '@/domain/errors'
+import { Router } from 'react-router-dom'
+import { ApiContext } from '@/presentation/contexts'
+import { createMemoryHistory } from 'history'
 
 class LoadSurveyListSpy implements LoadSurveyList {
   callsCount = 0
@@ -20,7 +23,13 @@ type SutTypes = {
 }
 
 const makeSut = (loadSurveyListSpy = new LoadSurveyListSpy()): SutTypes => {
-  render(<SurveyList loadSurveyList={loadSurveyListSpy} />)
+  render(
+  <ApiContext.Provider value={{ setCurrentAccount: jest.fn() }}>
+    <Router history={createMemoryHistory()}>
+      <SurveyList loadSurveyList={loadSurveyListSpy} />
+    </Router>
+  </ApiContext.Provider>
+  )
   return {
     loadSurveyListSpy
   }
