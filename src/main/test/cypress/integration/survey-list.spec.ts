373e587cc1612a6a6cec4e9a9bfc70ec57ca33f3
Alan <allan_net@live.com>
2022-05-24 19:48:42 -0300

    test: ensure SurveyList presents correct username on header

--- src/main/test/cypress/integration/survey-list.spec.ts
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -6,9 +6,23 @@ describe('SurveyList', () => {
   beforeEach(() => {
     Helper.setLocalStorageItem('account', { accessToken: faker.datatype.uuid(), name: faker.name.findName() })
   })
+
   it('Should present error on UnexpectedError', () => {
     Http.mockUnexpectedError()
     cy.visit('')
     cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
+  })
+
+  it('Should logout on AccessDeniedError', () => {
+    Http.mockAccessDeniedError()
+    cy.visit('')
+    Helper.testUrl('/login')
+  })
+
+  it('Should present correct username', () => {
+    Http.mockUnexpectedError()
+    cy.visit('')
+    const { name } = Helper.getLocalStorageItem('account')
+    cy.getByTestId('username').should('contain.text', name)
   })
 })
