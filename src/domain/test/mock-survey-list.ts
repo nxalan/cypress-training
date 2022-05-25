5e3acc2ac8c64a3edca126afa692ced06835d7db
Alan <allan_net@live.com>
2022-05-25 19:47:18 -0300

    refactor: remove unused code

--- src/domain/test/mock-survey-list.ts
+++ src/domain/test/mock-survey-list.ts
@@ -13,13 +13,3 @@ export const mockSurveyListModel = (): LoadSurveyList.Model[] => ([
   mockSurveyModel(),
   mockSurveyModel()
 ])
-
-export class LoadSurveyListSpy implements LoadSurveyList {
-  callsCount = 0
-  surveys = mockSurveyListModel()
-
-  async loadAll (): Promise<LoadSurveyList.Model[]> {
-    this.callsCount++
-    return this.surveys
-  }
-}
