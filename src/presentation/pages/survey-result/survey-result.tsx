ef5b6f71592077ff8223fed9d1b746a7c464e446
Alan <allan_net@live.com>
2022-05-28 20:39:58 -0300

    refactor: move Loading to its own file

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Styles from './survey-result-styles.scss'
 import FlipMove from 'react-flip-move'
-import { Footer, Header, Spinner } from '@/presentation/components'
+import { Footer, Header, Loading } from '@/presentation/components'
 
 const SurveyResult: React.FC = () => {
   return (
@@ -17,12 +17,7 @@ const SurveyResult: React.FC = () => {
           </li>
         </FlipMove>
         <button>Voltar</button>
-        <div className={Styles.loadingWrap}>
-          <div className={Styles.loading}>
-            <span>Aguarde...</span>
-            <Spinner isNegative/>
-          </div>
-        </div>
+        { false && (<Loading />)}
       </div>
       <Footer />
     </div>
