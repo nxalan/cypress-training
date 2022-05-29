28294aa0cc3ef93af470161324cfc91c2bbcccd2
Alan <allan_net@live.com>
2022-05-28 22:24:28 -0300

    feat: add skeleton with shimmer on SurveyResult

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -8,19 +8,23 @@ const SurveyResult: React.FC = () => {
     <div className={Styles.surveyResultWrap}>
       <Header />
       <div className={Styles.contentWrap}>
-        <hgroup>
-        <Calendar date={new Date()} className={Styles.calendarWrap} />
-        <h2>Qual é seu framework web favorito?</h2>
-        </hgroup>
-        <FlipMove className={Styles.answersList}>
-          <li>
-            <img src="http://fordevs.herokuapp.com/static/img/logo-react" />
-            <span className={Styles.answer}>React</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
-        </FlipMove>
-        <button>Voltar</button>
-        { false && (<Loading />)}
+        {false && (
+          <>
+            <hgroup>
+              <Calendar date={new Date()} className={Styles.calendarWrap} />
+              <h2>Qual é seu framework web favorito?</h2>
+            </hgroup>
+            <FlipMove className={Styles.answersList}>
+              <li>
+                <img src="http://fordevs.herokuapp.com/static/img/logo-react" />
+                <span className={Styles.answer}>React</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+            </FlipMove>
+            <button>Voltar</button>
+          </>
+        )}
+        {false && (<Loading />)}
       </div>
       <Footer />
     </div>
